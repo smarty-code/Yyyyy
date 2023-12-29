f69e36521af9fd4d01a9e5fa31292eb96d795cf2
smarty amit <107742291+smarty-code@users.noreply.github.com>
2023-12-29 17:40:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
-
+<script type="text/javascript">
+    var app_url = 'https://instantlinks.in/';
+    var app_api_token = 'f87424b3b05c522df2386414d92060bfad3b839e';
+    var app_advert = 1;
+    var app_domains = ["https://yyyyy-xtef.vercel.app/"];
+</script>
+<script src='//instantlinks.in/js/full-page-script.js'></script>
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
 });
